1adeaed194cd60f5cded568b0a8a824da3988e45
04005 <0321aquos@gmail.com>
2021-10-04 23:25:20 +0900

    Initial commit

---
+++ Assets/GameScript.cs
@@ -0,0 +1,22 @@
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameScript : MonoBehaviour
+{
+    public int score = 20;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
